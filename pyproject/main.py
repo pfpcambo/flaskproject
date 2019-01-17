dd92eac848e028301a4ffea82db519835fcd2915
pfpcambo <pfpcambo@gmail.com>
2019-01-17 15:42:32 +0700

    test

--- pyproject/main.py
+++ pyproject/main.py
@@ -1,17 +1,1 @@
-from mymodule.mod1 import mod1_name_1,func3 as f3,func4
-mod1_name_1="Makara"
-f3()
-func4(mod1_name_1)
-
-from pckPhone import *
-sName()
-sModel("N51")
-import math
-print(__name__)
-print(__file__)
-
-print(__package__)
-import pckPhone
-print(dir(pckPhone))
-print(locals())
-print(globals())
+print("test")
